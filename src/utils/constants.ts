09ec906f156303efaf0a500474b8760d8b27cf0d
Andrey Raih <andreyraih@yandex.ru>
2022-03-26 23:23:58 +0530

    re-apply .gitignore

--- src/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const LOCAL_STORAGE_CURRENCY_QUERY_KEY = 'app:currency-query'
-export const GENERIC_ERROR_TEXT = 'Somethings went wrong. Please, lets try again later'
-
-export const API_ENDPOINTS = {
-  CONVERT: '/convert',
-  SYMBOLS: '/symbols',
-  RATES: '/latest'
-}
